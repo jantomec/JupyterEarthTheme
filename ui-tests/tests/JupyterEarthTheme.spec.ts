f64ec94acb6c19cc10b050176f1d487988c24398
Jan Tomec <jan.tomec@gmail.com>
2024-04-01 21:42:57 +0200

    Prettier

--- ui-tests/tests/JupyterEarthTheme.spec.ts
+++ ui-tests/tests/JupyterEarthTheme.spec.ts
@@ -16,6 +16,8 @@ test('should emit an activation console message', async ({ page }) => {
   await page.goto();
 
   expect(
-    logs.filter(s => s === 'JupyterLab extension JupyterEarthTheme is activated!')
+    logs.filter(
+      s => s === 'JupyterLab extension JupyterEarthTheme is activated!'
+    )
   ).toHaveLength(1);
 });
